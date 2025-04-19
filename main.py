a75f33041e2d0b605cca20c1bdfa9f3ff7be9d3a
ItsGabeh <itsbielrt@gmail.com>
2025-04-18 20:27:17 -0600

    Add code to create the app's dir

--- main.py
+++ main.py
@@ -10,6 +10,30 @@
     Returns the password for the given email
 """
 import argparse
+import pathlib
+import os
+
+# Create the program directory if not exists
+# Get the user home directory
+home = pathlib.Path.home()
+
+# program's directory
+passpy_dir = ".passpy"
+
+# path to passpy dir
+passpy_dir_path = pathlib.Path(home / passpy_dir)
+
+# check if the passpy dir exists
+if not passpy_dir_path.exists():
+    print(f"passpy dir does not exists: {passpy_dir_path}")
+    print(f"creating passpy dir at {passpy_dir_path}")
+
+    # create the directory
+    # exists_ok prevents error if in any case the dir exists
+    passpy_dir_path.mkdir(exist_ok=True)
+
+    # change permissions: only owner can read, write and execute
+    os.chmod(passpy_dir_path, 0o700)
 
 # Add specifications to the parser
 parser = argparse.ArgumentParser(
